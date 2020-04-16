d59254dd759a82eae923daeba3e37fcecdea36fd
akeenkarkare <61316992+akeenkarkare@users.noreply.github.com>
2020-04-16 15:44:32 +0530

    Add files via upload
    Instructions will be provided throughout the game itself.

---
+++ madlib.py
@@ -0,0 +1,7 @@
+#Madlib.py
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+verb = input("Please enter a verb ending with -ed: ")
+animal = input("Please enter the name of an animal: ")
+print("Your Madlib:")
+print("The", adjective, noun, verb, "over the lazy", animal )
